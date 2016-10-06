d9558aeefc08698da85b22ec57e7634105232a4d
qianngchn <qianngchn@gmail.com>
2016-10-06 20:18:08 +0800

    fix missing check html

--- html_check.sh
+++ html_check.sh
@@ -3,13 +3,13 @@
 src=$1
 
 parent_path=`dirname $src`
-locallinks=`sed -n 's/.*\[.*\](\(.*\)).*/\1/p' $src | grep -v "http" | grep -v "mailto"`
-locallinks+=`sed -n 's/.*\[.*\]:\(.*\).*/\1/p' $src | grep -v "http" | grep -v "mailto"`
+locallinks=`sed -n 's/.*\[.*\](\(.*\)).*/\1/p' $src | awk '//{printf " %s\n", $1}' | grep -v "http" | grep -v "mailto"`
+locallinks+=`sed -n 's/.*\[.*\]:\(.*\).*/\1/p' $src | awk '//{printf " %s\n", $1}' | grep -v "http" | grep -v "mailto"`
 
 echo "Checking $src"
 for link in $locallinks
 do
-    if [[ $link == */* ]] && [ ! -f "$parent_path/$link" ]; then
+    if [ ! -f "$parent_path/$link" ]; then
         echo "$parent_path/$link does not exsit."
         exit 1
     fi
